344a0aed4a0edbac0664b5586710ba77b1725997
Ibrahim Oluwapeluwa <54927381+iPeluwa@users.noreply.github.com>
2023-07-13 01:33:55 +0100

    Cleanup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Pyresponse',
-    version='0.1.0',
+    version='0.1.1',
     description='Package for creating success and error responses in Python Projects',
     author='Ibrahim Oluwapeluwa',
     author_email='ipeluwa@gmail.com',
